c38bd06323cd25032f5dcde2f9790bcb2049607c
joacojachow <joacojachow@hotmail.com>
2026-03-25 12:03:51 -0300

    cleaner

--- hooks/use-toast.ts
+++ hooks/use-toast.ts
@@ -132,14 +132,12 @@ type Toast = Omit<ToasterToast, 'id'>
 
 function toast({ ...props }: Toast) {
   const id = genId()
-
   const update = (props: ToasterToast) =>
     dispatch({
       type: 'UPDATE_TOAST',
       toast: { ...props, id },
     })
   const dismiss = () => dispatch({ type: 'DISMISS_TOAST', toastId: id })
-
   dispatch({
     type: 'ADD_TOAST',
     toast: {
@@ -151,7 +149,6 @@ function toast({ ...props }: Toast) {
       },
     },
   })
-
   return {
     id: id,
     dismiss,
@@ -161,7 +158,6 @@ function toast({ ...props }: Toast) {
 
 function useToast() {
   const [state, setState] = React.useState<State>(memoryState)
-
   React.useEffect(() => {
     listeners.push(setState)
     return () => {
@@ -171,7 +167,6 @@ function useToast() {
       }
     }
   }, [state])
-
   return {
     ...state,
     toast,
